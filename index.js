b4fa0eb7c11bb06d6b5572606cc2ea5a33478e2d
Mike Terry <support@miketerry.org>
2025-05-20 00:57:00 -0500

    first version published to npm

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 
 // load all necessary modules
 const config = require("./lib/config.js");
+const cmdLine = require("./lib/cmdLine.js");
+
+// process an command line parameters
+cmdLine.run();
 
 // export all public types
-module.exports = { config };
+module.exports = config;
